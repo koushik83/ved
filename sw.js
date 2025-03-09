e18c0d27a9df06cf71c186664389787ccb2f4bfb
Chandan <rastogi.chandan@gmail.com>
2025-03-09 10:20:54 +0530

    installation app feature added

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener("install", () => {
+    console.log("Service Worker Installed");
+  });
+  
+  self.addEventListener("fetch", (event) => {
+    event.respondWith(fetch(event.request));
+  });
+  
